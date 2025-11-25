eba5a6dec40464d1193d090ea86ac6779215fa3b
rameshsuhasini <suhasiniramesh20@gmail.com>
2025-11-25 13:01:20 +0100

    Feature: Added the Offers list Page working component with UI

--- src/app/features/offers/pages/offers-list-page/offers-list-page.component.ts
+++ src/app/features/offers/pages/offers-list-page/offers-list-page.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { OfferCardComponent } from '../../../../shared/components/offer-card/offer-card.component';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { Offer } from '../../../../core/models/offer.model';
+import { OfferApiService } from '../../../../core/api/offer-api.service';
 @Component({
   selector: 'app-offers-list-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, OfferCardComponent, RouterModule],
   templateUrl: './offers-list-page.component.html',
-  styleUrl: './offers-list-page.component.scss'
+  styleUrl: './offers-list-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OffersListPageComponent {
+  offers$: Observable<Offer[]> = this.offerApi
+    .getOffers()
+    .pipe(map((offers) => [...offers].sort((a, b) => b.votes - a.votes)));
 
+  constructor(private offerApi: OfferApiService,
+     private router: Router) {}
+
+     trackById(index: number, offer: Offer): number {
+      return  offer.id;
+     }
+
+     OnUpVote(id: number): void {
+      console.log('Upvote', id);
+     }
+      OnDownVote(id: number): void {
+      console.log('Upvote', id);
+     }
+      OnViewDetails(id: number): void {
+      console.log('Upvote', id);
+       this.router.navigate(['/offers', id]);
+     }
 }
