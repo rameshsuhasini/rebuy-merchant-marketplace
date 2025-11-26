4e3195f7d64303f8690238f67242eb4606f2f7b8
rameshsuhasini <suhasiniramesh20@gmail.com>
2025-11-26 12:20:24 +0100

    Point UI to the API Gateway URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://rebuy-merchant-backend-env.eba-j9mdrzfg.eu-north-1.elasticbeanstalk.com' 
+  apiBaseUrl: 'https://um0db1uj62.execute-api.eu-north-1.amazonaws.com' 
 };
